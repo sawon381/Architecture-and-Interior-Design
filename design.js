f6ccbc522884e99f947788c2e656eb012d15c63b
sawon381 <74038798+sawon381@users.noreply.github.com>
2020-11-07 19:42:35 +0600

    Add files via upload

---
+++ design.js
@@ -0,0 +1,33 @@
+
+
+window.onload = () =>{
+	setTimeout(() =>{
+		document.querySelector('body').classList.add("display")
+	},1000);
+}
+
+
+
+
+
+
+
+document.querySelector('.humberger_menu').addEventListener('click',()=>{
+	
+	document.querySelector(".container").classList.toggle("change");
+});
+
+
+document.querySelector('.scroll_btn').addEventListener("click",() =>{
+	document.querySelector("html").style.scrollBehavior = "smooth";
+		setTimeout(() =>{
+		document.querySelector("html").style.scrollBehavior = "unset";
+},1000);
+});
+
+
+
+
+
+
+
